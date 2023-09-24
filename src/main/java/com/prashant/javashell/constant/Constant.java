fec24ea28f852672fab58028b4433db0aa34902d
prashantkushwaha <prashantkushwaha563@gmail.com>
2023-09-24 14:00:44 +0530

    executable file added

--- src/main/java/com/prashant/javashell/constant/Constant.java
+++
@@ -1,7 +0,0 @@
-package com.prashant.javashell.constant;
-
-public class Constant {
-    public static final String GOOGLE_URL="https://www.google.com";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_RESET = "\u001B[0m";
-}
